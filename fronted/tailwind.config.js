794eaceccbe9a8fb40c21c5f3154367c746bad5a
manish_singh <manishkandari43@gmail.com>
2024-11-24 21:54:19 +0530

    code

--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -1,9 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-blue': '#00ffff',
+        'neon-green': '#00ff00',
+        'neon-red': '#ff0000',
+        'neon-yellow': '#ffff00',
+        'neon-orange': '#ff6600',
+        'neon-purple': '#9900ff',
+        'neon-pink': '#ff00ff', // Included neon-pink once
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
-
